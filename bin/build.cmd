e4951b621030c31b07f0f2bf65bdc78a39b841dd
XSG <56810813+xsgam@users.noreply.github.com>
2026-03-25 14:16:08 +0700

    Update build.cmd

--- bin/build.cmd
+++ bin/build.cmd
@@ -1,9 +1,1 @@
 @echo off
-echo Building MKCert...
-go build -ldflags="-s -w" -o .\bin\MKCert.exe main.go
-if %ERRORLEVEL% EQU 0 (
-    echo Build successful: MKCert.exe
-) else (
-    echo Build failed!
-    exit /b 1
-)
